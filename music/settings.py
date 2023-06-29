837d1b5a4eb52789e4ee86e6fd4472c4acbe17a3
leehjhjhj <tsukiakarii@naver.com>
2023-06-29 23:49:26 +0900

    fix: setting holder change

--- music/settings.py
+++ music/settings.py
@@ -9,7 +9,7 @@ CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'https://musicprofile.kro.kr']
 SECRET_KEY = config('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
