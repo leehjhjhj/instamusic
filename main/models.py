30e5592b41cf55c12afc4a4d42a554122b418678
leehjhjhj <tsukiakarii@naver.com>
2023-06-30 11:22:07 +0900

    chore: model image field 수정

--- main/models.py
+++ main/models.py
@@ -6,5 +6,5 @@ class Song(models.Model):
     user = models.ForeignKey(User,related_name='song_user', on_delete=models.CASCADE, default='')
     name = models.CharField(max_length=20)
     artist = models.CharField(max_length=20)
-    thumbnail = models.ImageField(upload_to='music_thumbnails/', null=True)
+    thumbnail = models.CharField(max_length=256, null=True)
     url = models.CharField(max_length=256)
