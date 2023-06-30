eccc59c6a950e5a7e744213939e9bf0fb349b2d4
leehjhjhj <tsukiakarii@naver.com>
2023-06-30 11:29:01 +0900

    chore: change model name and artist

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from accounts.models import User
 # Create your models here.
 class Song(models.Model):
     user = models.ForeignKey(User,related_name='song_user', on_delete=models.CASCADE, default='')
-    name = models.CharField(max_length=20)
-    artist = models.CharField(max_length=20)
+    name = models.CharField(max_length=128)
+    artist = models.CharField(max_length=128)
     thumbnail = models.CharField(max_length=256, null=True)
     url = models.CharField(max_length=256)
